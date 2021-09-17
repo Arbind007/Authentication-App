3a045242af197351c3441bc5ea41ed8b2d02ac8c
Arbind007 <almbbsr456@gmail.com>
2021-09-17 13:07:01 +0530

    :bug: using env PORT

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 mongoose.connect(
